a1569a9a81d943280e5a92e9a4f2ee755a222d8c
Anthony <anthonyharisson30@gmail.com>
2022-06-20 15:10:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function msg(){
+    console.log('Estou exibindo uma mensagem no console!');
+}
+msg();
+
+function nome(nome){
+    console.log(`Estou exibindo meu nome: ${nome}`);
+}
+nome('Anthony');
+
+function dados(nome, idade, estiloMusical){
+    console.log(`Olá, Meu nome é ${nome}, tenho ${idade} anos e adoro ouvir ${estiloMusical}!`);
+}
+dados('Anthony', '21', 'Trap');
+
+function prefer(filme, musica){
+    console.log(`Meu filme e musica predileta são ${filme} e ${musica}!`);
+}
+
+prefer('Kimi no na wa', 'No Guidance - Chris Brown');
+
+function conta(numero){
+    console.log(3 * numero);
+}
+conta(50);
